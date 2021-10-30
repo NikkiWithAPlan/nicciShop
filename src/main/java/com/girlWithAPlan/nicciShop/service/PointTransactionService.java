adc9f89de583d283566d9678ac41597f6c901415
Nikki Szedljak <nikoletta.szedljak@gmail.com>
2021-10-30 17:00:06 +0100

    Set up integration test

--- src/main/java/com/girlWithAPlan/nicciShop/service/PointTransactionService.java
+++ src/main/java/com/girlWithAPlan/nicciShop/service/PointTransactionService.java
@@ -1,7 +1,22 @@
 package com.girlWithAPlan.nicciShop.service;
 
+import com.girlWithAPlan.nicciShop.entity.PointTransaction;
+import com.girlWithAPlan.nicciShop.repository.PointTransactionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointTransactionService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PointTransaction.class);
+
+    private final PointTransactionRepository pointTransactionRepository;
+
+    @Autowired
+    public PointTransactionService(PointTransactionRepository pointTransactionRepository) {
+        this.pointTransactionRepository = pointTransactionRepository;
+    }
+
 }
--- src/test/java/com/girlWithAPlan/nicciShop/service/PointTransactionServiceIntegrationTest.java
+++ src/test/java/com/girlWithAPlan/nicciShop/service/PointTransactionServiceIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.girlWithAPlan.nicciShop.service;
 
+import com.girlWithAPlan.nicciShop.repository.PointTransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
@@ -7,4 +9,10 @@ import javax.transaction.Transactional;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Transactional
 public class PointTransactionServiceIntegrationTest {
+
+    @Autowired
+    private PointTransactionService pointTransactionService;
+
+    @Autowired
+    private PointTransactionRepository pointTransactionRepository;
 }
