878232b86317284926708feef0b47eeaa6cef3fc
Nikki Szedljak <nikoletta.szedljak@gmail.com>
2021-10-30 14:19:35 +0100

    Initial commit

---
+++ src/main/java/com/girlWithAPlan/nicciShop/NicciShopApplication.java
@@ -0,0 +1,13 @@
+package com.girlWithAPlan.nicciShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicciShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicciShopApplication.class, args);
+	}
+
+}
