68604eb69bddd51430971a37ef57e3654fc5ba43
Nikki Szedljak <nikoletta.szedljak@gmail.com>
2021-10-31 11:26:51 +0000

    Implemented new balance logic

--- src/main/java/com/girlWithAPlan/nicciShop/service/PointTransactionService.java
+++ src/main/java/com/girlWithAPlan/nicciShop/service/PointTransactionService.java
@@ -2,6 +2,7 @@ package com.girlWithAPlan.nicciShop.service;
 
 import com.girlWithAPlan.nicciShop.entity.PointTransaction;
 import com.girlWithAPlan.nicciShop.entity.Shopper;
+import com.girlWithAPlan.nicciShop.entity.TransactionStatus;
 import com.girlWithAPlan.nicciShop.repository.PointTransactionRepository;
 import com.girlWithAPlan.nicciShop.repository.ShopperRepository;
 import org.slf4j.Logger;
@@ -31,6 +32,12 @@ public class PointTransactionService {
 
         Shopper shopper = shopperRepository.findById(shopperId)
                 .orElseThrow(() -> new NoSuchElementException("Shopper not found for id=" + shopperId));
+
+        LOGGER.info("Update Shopper balance");
+        if (TransactionStatus.COMPLETED.equals(pointTransaction.getStatus())) {
+            shopper.setBalance(shopper.getBalance().add(pointTransaction.getPointAmount()));
+            LOGGER.info("New Shopper.balance={}", shopper.getBalance());
+        }
 
         pointTransaction.setShopper(shopper);
 
